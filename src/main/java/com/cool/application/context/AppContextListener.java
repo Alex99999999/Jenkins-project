2514b16acc064a624bb8ed9ff22746725787bdb0
Oleksandr Hlushchenko <oleksandr_hlushchenko@epam.com>
2022-05-27 17:11:36 +0300

    fill application context

--- src/main/java/com/cool/application/context/AppContextListener.java
+++ src/main/java/com/cool/application/context/AppContextListener.java
@@ -1,6 +1,9 @@
 package com.cool.application.context;
 
 import com.cool.application.context.impl.PostgresApplicationContextImpl;
+import com.cool.application.db.DbConnectionProvider;
+import com.cool.application.db.postgres.PostgresDbConnectionProviderImpl;
+import com.cool.application.servlet.attributes.GlobalAttributes;
 import com.cool.application.servlet.webcommand.CommandContainer;
 
 import javax.servlet.ServletContext;
@@ -15,21 +18,21 @@ public class AppContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext ctx = sce.getServletContext();
-        Connection con = null;
-
         //TODO Establish db connection
+        DbConnectionProvider dbConnectionProvider = new PostgresDbConnectionProviderImpl();
+        Connection con = dbConnectionProvider.getConnection();
 
-        ApplicationContext applicationContext = new PostgresApplicationContextImpl();
+        ApplicationContext applicationContext = new PostgresApplicationContextImpl(dbConnectionProvider);
         CommandContainer commandContainer = new CommandContainer(applicationContext);
-        ctx.setAttribute("DbConnection", con);
-        ctx.setAttribute("CommandContainer", commandContainer);
+        ctx.setAttribute(GlobalAttributes.DB_CONNECTION, con);
+        ctx.setAttribute(GlobalAttributes.COMMAND_CONTAINER, commandContainer);
 
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
         ServletContext ctx = servletContextEvent.getServletContext();
-        Connection con = (Connection) ctx.getAttribute("DbConnection");
+        Connection con = (Connection) ctx.getAttribute(GlobalAttributes.DB_CONNECTION);
 
         //TODO Close DB connection
 
--- src/main/java/com/cool/application/context/impl/PostgresApplicationContextImpl.java
+++ src/main/java/com/cool/application/context/impl/PostgresApplicationContextImpl.java
@@ -2,17 +2,18 @@ package com.cool.application.context.impl;
 
 import com.cool.application.dao.UserDao;
 import com.cool.application.dao.postgresimpl.UserDaoImpl;
+import com.cool.application.db.DbConnectionProvider;
 import com.cool.application.service.UserService;
 import com.cool.application.service.postgresimpl.UserServiceImpl;
 import com.cool.application.context.ApplicationContext;
 
 public class PostgresApplicationContextImpl implements ApplicationContext {
 
-    private UserService userService;
-    private UserDao userDao;
+    private final UserDao userDao;
+    private final UserService userService;
 
-    public PostgresApplicationContextImpl() {
-        this.userDao = new UserDaoImpl();
+    public PostgresApplicationContextImpl(DbConnectionProvider connectionProvider) {
+        this.userDao = new UserDaoImpl(connectionProvider);
         this.userService = new UserServiceImpl(userDao);
     }
 
