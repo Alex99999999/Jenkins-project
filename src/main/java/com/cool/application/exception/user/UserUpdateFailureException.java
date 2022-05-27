9b4583cf780a70fdfd5c8bd5b87873ece85f0cfc
Oleksandr Hlushchenko <oleksandr_hlushchenko@epam.com>
2022-05-27 17:13:25 +0300

    add user exceptions

---
+++ src/main/java/com/cool/application/exception/user/UserUpdateFailureException.java
@@ -0,0 +1,9 @@
+package com.cool.application.exception.user;
+
+public class UserUpdateFailureException extends RuntimeException {
+
+    public UserUpdateFailureException(String message) {
+        super(message);
+    }
+
+}
