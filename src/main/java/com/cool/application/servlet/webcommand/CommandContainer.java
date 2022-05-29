524f85091e9ccebd4e913a8d7f27758121fee040
ArtemDobro <ArtemDobrohotov2002@gmail.com>
2022-05-29 17:07:38 +0200

    add to populateCommands

--- src/main/java/com/cool/application/servlet/webcommand/CommandContainer.java
+++ src/main/java/com/cool/application/servlet/webcommand/CommandContainer.java
@@ -2,6 +2,7 @@ package com.cool.application.servlet.webcommand;
 
 import com.cool.application.context.ApplicationContext;
 import com.cool.application.operations.UserOperations;
+import com.cool.application.servlet.webcommand.impl.CreateUserCommand;
 import com.cool.application.servlet.webcommand.impl.DefaultCommand;
 import com.cool.application.servlet.webcommand.impl.GetUserByIdCommand;
 import com.cool.application.servlet.webcommand.impl.UpdateUserCommand;
@@ -27,6 +28,7 @@ public class CommandContainer {
         commands = new HashMap<>();
         commands.put(UserOperations.UPDATE_USER.getOperationName(), new UpdateUserCommand(applicationContext.getUserService()));
         commands.put(UserOperations.GET_USER_BY_ID.getOperationName(), new GetUserByIdCommand(applicationContext.getUserService()));
+        commands.put(UserOperations.CREATE_USER.getOperationName(), new CreateUserCommand(applicationContext.getUserService()));
     }
 
 }
