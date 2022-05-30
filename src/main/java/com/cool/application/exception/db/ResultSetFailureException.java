99e1b275519d484d0f945028c7ca50437d6745f6
Bodan <bogdan.yakovenko.301@gmail.com>
2022-05-30 14:15:43 +0300

    Added find all users and find user by family name commands.

---
+++ src/main/java/com/cool/application/exception/db/ResultSetFailureException.java
@@ -0,0 +1,6 @@
+package com.cool.application.exception.db;
+
+public class ResultSetFailureException extends RuntimeException{
+
+    public ResultSetFailureException(String message){super(message);}
+}
