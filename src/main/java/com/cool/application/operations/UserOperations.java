6154a087a113a9d0be8f1cba8151bb18f92888ae
Oleksandr Hlushchenko <oleksandr_hlushchenko@epam.com>
2022-05-30 15:10:43 +0300

    added GET_USER_BY_NAME operation deleted in the course of merge conflict resolution

--- src/main/java/com/cool/application/operations/UserOperations.java
+++ src/main/java/com/cool/application/operations/UserOperations.java
@@ -1,21 +1,21 @@
 package com.cool.application.operations;
 
 public enum UserOperations {
-  UPDATE_USER("update_user"),
-  CREATE_USER("create_user"),
-  GET_USER_BY_ID("get_user_by_id"),
-  DELETE_USER("delete_user"),
-  GET_ALL_USERS("get_all_users");
+    UPDATE_USER("update_user"),
+    CREATE_USER("create_user"),
+    GET_USER_BY_ID("get_user_by_id"),
+    DELETE_USER("delete_user"),
+    GET_ALL_USERS("get_all_users"),
+    GET_USER_BY_NAME("get_user_by_name");
 
+    private final String query;
 
-  private final String query;
+    UserOperations(String query) {
+        this.query = query;
+    }
 
-  UserOperations(String query) {
-    this.query = query;
-  }
-
-  public String getOperationName() {
-    return query;
-  }
+    public String getOperationName() {
+        return query;
+    }
 
 }
