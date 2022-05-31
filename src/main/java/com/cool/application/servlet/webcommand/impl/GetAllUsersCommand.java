07a15737a7a6061ad492eba6995ff3b32dc6fd0b
Oleksandr Hlushchenko <oleksandr_hlushchenko@epam.com>
2022-05-31 18:07:42 +0300

    test db

--- src/main/java/com/cool/application/servlet/webcommand/impl/GetAllUsersCommand.java
+++ src/main/java/com/cool/application/servlet/webcommand/impl/GetAllUsersCommand.java
@@ -19,9 +19,10 @@ public class GetAllUsersCommand implements Command {
 
     @Override
     public String execute(HttpServletRequest req) {
-        List<User> users = userService.findAllUsers();
-        req.setAttribute(GlobalAttributes.USER_LIST, users);
         return Pages.SHOW_ALL_USERS;
+
+//        List<User> users = userService.findAllUsers();
+//        req.setAttribute(GlobalAttributes.USER_LIST, users);
     }
 
 }
