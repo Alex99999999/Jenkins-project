a7fc79e33b2c7fb9bc82450e8d177a568f132202
Oleksandr Hlushchenko <oleksandr_hlushchenko@epam.com>
2022-05-31 20:44:01 +0300

    test db

--- src/main/java/com/cool/application/db/postgres/PostgresDbConnectionProviderImpl2.java
+++ src/main/java/com/cool/application/db/postgres/PostgresDbConnectionProviderImpl2.java
@@ -14,7 +14,6 @@ public class PostgresDbConnectionProviderImpl2 implements DbConnectionProvider {
 
         try {
             Class.forName("org.postgresql.Driver");
-
             connection = DriverManager.getConnection("jdbc:postgresql://192.168.1.193:5432/coolcatsDB", "postgres", "postgres");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
@@ -24,8 +23,6 @@ public class PostgresDbConnectionProviderImpl2 implements DbConnectionProvider {
         // When this class first attempts to establish a connection, it automatically loads any JDBC 4.0 drivers found within
         // the class path. Note that your application must manually load any JDBC drivers prior to version 4.0.
 //          Class.forName("org.postgresql.Driver");
-
-
 //        catch (ClassNotFoundException e) {
 //            System.out.println("PostgreSQL JDBC driver not found.");
 //            e.printStackTrace();
