10a8d0257797f56cda7c6bcca0e216a40245ac41
Oleksandr Hlushchenko <oleksandr_hlushchenko@epam.com>
2022-06-02 15:03:59 +0300

    test db

--- src/main/java/com/cool/application/context/AppContextListener.java
+++ src/main/java/com/cool/application/context/AppContextListener.java
@@ -20,7 +20,9 @@ public class AppContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext ctx = sce.getServletContext();
-        DbConnectionProvider dbConnectionProvider = new Test();
+//        DbConnectionProvider dbConnectionProvider = new Test(); //Hard coded DB creds
+        DbConnectionProvider dbConnectionProvider = new PostgresDbConnectionProviderImpl();
+
         Connection con = dbConnectionProvider.getConnection();
 
         ApplicationContext applicationContext = new PostgresApplicationContextImpl(dbConnectionProvider);
